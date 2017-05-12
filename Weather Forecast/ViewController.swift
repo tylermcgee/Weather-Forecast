b83e5f54a79df0f1aaafc7ffb04c5678f0bebd17
Syngenta Mac <syngentardismac@Syngentas-MacBook-Pro.local>
2017-05-12 10:19:47 -0400

    Initial Commit

---
+++ Weather Forecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Forecast
+//
+//  Created by Syngenta RDIS Mac on 5/12/17.
+//  Copyright © 2017 Syngenta GenEx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
